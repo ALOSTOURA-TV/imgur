568ba96734a36699094129aa27bbe5d26caaeda7
الأسطورة <158589821+ALOSTOURA-TV@users.noreply.github.com>
2025-06-21 19:07:55 +0300

    Update generate_files_json.py

--- generate_files_json.py
+++ generate_files_json.py
@@ -8,7 +8,7 @@ files = [
     if f.lower().endswith(allowed_exts) and os.path.isfile(f)
 ]
 
-files.sort()
+files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
 
 with open('files.json', 'w', encoding='utf-8') as f:
     json.dump(files, f, indent=2, ensure_ascii=False)
